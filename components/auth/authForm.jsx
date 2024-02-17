193aa1c2e0f1028765bf898e7e60bc26d2e546cd
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-02-17 10:47:15 +0200

    created auth form

--- components/auth/authForm.jsx
+++ components/auth/authForm.jsx
@@ -1,19 +1,97 @@
 import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
+import style from './form.module.css'
 import { useState } from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
 
-export default function AuthForm({children}){
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(0),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
-  const [setOpen, open ] = useState(true)
+export default function AuthForm() {
+
+  const [open, setOpen] = useState(true)
+  const [image, setImage] = useState(null)
+
+  function convertToBase64(e) {
+
+    var reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = () => {
+      setImage(reader.result)
+    }
+    reader.onerror = error => {
+      console.log('Error: ', error)
+    }
+  }
+
+  function signupHandler(e) {
+    e.preventDefault()
+    const formData = new FormData(e.target);
+
+    const userData = {
+      username: formData.get('username'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      image: image
+    }
+
+    console.log(userData)
+
+  }
 
   return (
-    <div>
+    <div className={style.main}>
       <Backdrop
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={open}
-        // onClick={handleClose}
       >
-        <CircularProgress/>
+        <form onSubmit={signupHandler} action="#" className={style.form}>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={{ xs: 2, md: 2 }}>
+              <Grid xs={12} md={12} s={2} >
+                <Item>
+                  <input type='username' name="username" placeholder="Username" required />
+                </Item>
+              </Grid>
+
+              <Grid xs={12} md={12} s={2}>
+                <Item>
+                  <input type='email' name="email" placeholder="email" required />
+                </Item>
+              </Grid>
+
+              <Grid xs={12} md={12} s={2}  >
+                <Item>
+                  <input type='password' name="password" placeholder="Password" required />
+                </Item>
+              </Grid>
+
+              <Grid xs={8} md={8} s={2}  >
+                <label>Profile Image. Optional</label>
+                <Item>
+                  <input onChange={convertToBase64} type='file' placeholder="Image" />
+                </Item>
+              </Grid>
+
+              <Grid xs={3} md={3} s={2}  >
+                <Item style={{
+                  background: 'transparent',
+                  boxShadow: 'none',
+                  textAlign: 'center'
+                }}>
+                  <button className={style.button} type="submit">Sign Up</button>
+                </Item>
+              </Grid>
+            </Grid>
+          </Box>
+        </form>
       </Backdrop>
     </div>
   )
