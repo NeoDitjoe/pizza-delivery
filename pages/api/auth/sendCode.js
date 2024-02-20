b2777ccb6614cc1f0441014a3a7a9a22b533219b
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-02-20 17:37:16 +0200

    send reset pin to user

---
+++ pages/api/auth/sendCode.js
@@ -0,0 +1,18 @@
+import sendCode from "@/util/database/resetPassord/sendCode"
+import { sendCodeToEmail } from "@/util/mail"
+
+export default async function handler(req, res){
+
+  if(req.method === 'POST'){
+
+    const { email, code } = req.body
+
+    try {
+      await sendCode(email, code)
+      await sendCodeToEmail(email, code)
+      res.status(200).json({ message: 'success'})
+    } catch (error) {
+      res.status(500).json({ message: 'failed to send code'})
+    }
+  }
+}
