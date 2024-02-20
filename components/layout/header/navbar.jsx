2ee51c979ea238782d7ed6eb69ed80836b220d22
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-02-20 08:41:22 +0200

    user sign out

--- components/layout/header/navbar.jsx
+++ components/layout/header/navbar.jsx
@@ -1,9 +1,22 @@
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-
+import { Fragment } from "react";
 
 export default function Navbar() {
-  
+
+  const { data: session } = useSession()
+
+  console.log(session)
+
   return (
-    <Link href='/auth'>Sign In</Link>
+    <Fragment>
+      {
+        !session
+         ? <Link href='/auth'>Sign In</Link>
+         :<button
+           onClick={() => signOut()}
+         >Sign Out</button>
+      }
+    </Fragment>
   )
 }
