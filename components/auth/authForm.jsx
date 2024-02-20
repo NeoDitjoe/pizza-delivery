1cfeb200390cbe61c7aa48595604f6bc6c4fa2b0
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-02-20 16:02:37 +0200

    added forgot password button

--- components/auth/authForm.jsx
+++ components/auth/authForm.jsx
@@ -146,7 +146,7 @@ export default function AuthForm() {
                   <input type={!showPassword ? 'password' : 'text'} name="password" placeholder="Password" required />
                   <div onClick={() => setShowdPassword(!showPassword)} >
                     {!showPassword
-                      ? <RiEyeLine/>
+                      ? <RiEyeLine />
                       : <RiEyeCloseLine />
                     }
                   </div>
@@ -183,12 +183,18 @@ export default function AuthForm() {
               </Grid>
 
               <Grid xs={12} md={12} s={2}>
-                <p
-                  className={style.haveAccount}
-                  onClick={() => setHaveAccount(!haveAccout)
-                  }>
-                  {haveAccout ? "Create account" : "Sign In"}
-                </p>
+                <Item style={{ background: 'transparent', boxShadow: 'none' }}>
+                  <p
+                    className={style.haveAccount}
+                    onClick={() => setHaveAccount(!haveAccout)
+                    }>
+                    {haveAccout ? "Create account" : "Sign In"}
+                  </p>
+                  <p
+                    className={style.haveAccount}
+                    onClick={() => router.push('/reset-password')}
+                  >Forgot password ?</p>
+                </Item>
               </Grid>
             </Grid>
           </Box>
