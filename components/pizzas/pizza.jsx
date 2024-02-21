6e05e32b923aa79fd4d37208786510648c86b11b
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-02-22 00:38:08 +0200

    created add to cart overlay and theme color

--- components/pizzas/pizza.jsx
+++ components/pizzas/pizza.jsx
@@ -4,6 +4,9 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import Image from 'next/image';
 import style from './pizzas.module.css'
+import { Backdrop } from '@mui/material';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -17,17 +20,21 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function ShowPizza(props) {
 
   const { data } = props
+  const [openOverlay, setOpenOverlay] = useState(false)
+
+  const router = useRouter()
+
   return (
     <div>
       <Box sx={{ flexGrow: 2 }}>
         <Grid
-          container spacing={{ xs: 1, md: 0 }}
+          container spacing={{ xs: 1, md: 1 }}
           columns={{ xs: 12, sm: 12, md: 12 }}
         >
           {
             data && data.map((pizza) => (
               <Grid xs={12} sm={4} md={4}>
-                <Item>
+                <Item className={style.item}>
                   <Image
                     src={pizza.image}
                     alt='pizza'
@@ -42,11 +49,20 @@ export default function ShowPizza(props) {
 
                   <div className={style.prices}>
                     {
-                      pizza.prices.map((pizza, y) => (
+                      pizza.prices.map((price, y) => (
                         <div >
                           {
-                            Object.entries(pizza).map(([key, value]) => (
-                              <div key={y}>
+                            Object.entries(price).map(([key, value]) => (
+                              <div
+                                className={style.size}
+                                key={y + value}
+                                onClick={() => {
+                                  router.push(`
+                                    ${router.query.pizzas}?name=${pizza.name.split(' ').join('-')}&size=${key + '-' + value}&base=${router.query.pizzas}&image=${pizza.image}&toppings=${pizza.toppings.join('-')}`)
+                                  console.log(pizza)
+                                  setOpenOverlay(true)
+                                }}
+                              >
                                 <h4>{key}</h4>
                                 <h6>R {Number(value).toFixed(2)}</h6>
                               </div>
@@ -62,6 +78,36 @@ export default function ShowPizza(props) {
           }
         </Grid>
       </Box>
+
+
+      {openOverlay && <Backdrop
+        sx={{ color: '#black', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={openOverlay}
+      >
+        <div className={style.backdrop}>
+          <p className={style.close} onClick={() => setOpenOverlay(false)}>close</p>
+          <Image
+            src={router.query.image}
+            alt={router.query.name}
+            width={300}
+            height={300}
+          />
+
+          <h3>{router.query.name}</h3>
+          <p>{router.query.toppings}</p>
+          <p>Size: {router.query.size && router.query.size.split('-')[0]}</p>
+
+          <button className={style.addToCart}>
+            Add to cart
+            Total cost: R {router.query.size && Number(router.query.size.split('-')[1]).toFixed(2)}
+          </button>
+
+          <div>
+            
+          </div>
+
+        </div>
+      </Backdrop>}
     </div>
   )
 }
