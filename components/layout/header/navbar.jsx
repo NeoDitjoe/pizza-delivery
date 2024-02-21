cca6dae4635f92e83ada2456089087541bdd2c42
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-02-21 07:58:17 +0200

    added pizza base options

--- components/layout/header/navbar.jsx
+++ components/layout/header/navbar.jsx
@@ -2,6 +2,9 @@ import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import style from './navbar.module.css'
 import { useRouter } from "next/router";
+import { Fragment } from "react";
+import pizzaBase from "@/util/pizza-base";
+import Image from "next/image";
 
 export default function Navbar() {
 
@@ -24,15 +27,39 @@ export default function Navbar() {
       .username.slice(1)
 
   return (
-    <div className={isHomePage ? style.background : style.backgroundB }>
+    <Fragment>
+      <div className={isHomePage ? style.background : style.backgroundB}>
 
-      <navbar className={style.navbar}>
-        {!session
-          ? <Link href='/auth'>Sign In</Link>
-          : <Link href={'/'}>Home</Link>
-        }
-        <Link href={'/user-profile'}>{username}</Link>
-      </navbar>
-    </div>
+        <navbar className={style.navbar}>
+          {!session
+            ? <Link href='/auth'>Sign In</Link>
+            : <Link href={'/'}>Home</Link>
+          }
+          <Link href={'/user-profile'}>{username}</Link>
+        </navbar>
+      </div>
+
+      <main className={style.main}>
+        <h2>Choose Your Pizza Base</h2>
+        <div className={style.pizzaBase}>
+          {
+            pizzaBase.map((item) => {
+              return (
+                <Link href={item.link}>
+                  <Image
+                    src={item.image}
+                    width={200}
+                    height={200}
+                    alt='pizza'
+                    className={style.img}
+                  />
+                  <h5>{item.name}</h5>
+                </Link>
+              )
+            })
+          }
+        </div>
+      </main>
+    </Fragment>
   )
 }
