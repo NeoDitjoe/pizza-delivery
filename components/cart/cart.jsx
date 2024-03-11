f20d8ca90bf1d9756e6a5ff46ed556e3552ce9a7
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-03-11 13:39:09 +0200

    empty cart

--- components/cart/cart.jsx
+++ components/cart/cart.jsx
@@ -1,10 +1,52 @@
+import { useRouter } from 'next/router'
+import style from './cart.module.css'
+import Backdrop from '@mui/material/Backdrop';
+import Image from 'next/image';
+import emptyPizzaBox from '../../public/2362491aea0ced8e46d5b276fe578783-removebg-preview.png'
 
-export default function Cart(props){
+export default function Cart(props) {
   const { data } = props
+  const router = useRouter()
 
   console.log(data)
 
-  return(
+  if (!data.length > 0) {
+    return (
+      <div>
+
+        <Backdrop
+          sx={{ color: 'black', background: 'transparent', marginTop: '60px', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={true}
+        >
+          <div style={{ textAlign: 'center'}}>
+            <div>
+              <Image 
+                src={emptyPizzaBox}
+                alt='empty'
+                width={140}
+                height={140}
+              />
+            </div>
+
+            <div>
+              <p>Your cart is empty</p>
+            </div>
+
+            <div>
+              <button
+                onClick={() => router.push('/Chicago Deep Dish Pizza Dough')}
+                className={style.button}
+              >
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </Backdrop>
+      </div>
+    )
+  }
+
+  return (
     'user cart'
   )
 }
