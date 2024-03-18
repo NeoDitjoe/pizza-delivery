36e00de85472e90eb7e3f8d7e8ba4faebec35031
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-03-18 03:06:22 +0200

    logo and meta

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,28 @@
 import Home from "@/components/home/home";
+import Head from "next/head";
 
 
 export default function HomePage() {
   return (
     <main>
+      <Head>
+        <meta name="description" content="Rock your taste buds with [Shop Name] Pizza! Our 
+        app lets you order delicious, customizable pizzas for delivery or quick pickup 
+        at locations across South Africa. Choose your crust, cheese, veggies, and sauce 
+        to create your perfect slice. Order now and experience pizza that rocks!"
+        />
+
+        <meta name="keywords"
+          content="Pizza App South Africa,
+            Pizza Delivery South Africa,
+            Order Pizza Online South Africa,
+            Customize Pizza Online South Africa,
+            Build Your Own Pizza South Africa,
+            Pizza Takeaway App South Africa,
+            Collect Pizza Order South Africa,
+            [Shop Name] Pizza App"
+        />
+      </Head>
       <Home />
     </main>
   );
