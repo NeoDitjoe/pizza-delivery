e0531b23f32f17135cb305c29ad1b6fa7c5e6f9d
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-03-18 08:32:35 +0200

    delete account

--- components/profile/profile.jsx
+++ components/profile/profile.jsx
@@ -1,30 +1,51 @@
 import { signOut, useSession } from 'next-auth/react'
 import style from './profile.module.css'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { CircularProgress } from '@mui/material'
 import TrackOrders from './track-orders.jsx/order'
+import deleteMethod from '@/util/deleteMethod'
+import stateContext from '@/util/context'
 
 export default function UserProfile(props) {
 
   const { orderId, orderDetails } = props
+  const { setAlert } = stateContext()
+
+  const [deleteLoader, setDeleteLoader] = useState(false)
 
   const router = useRouter()
   const { data: session, status } = useSession()
 
-  // useEffect(() => {
-  //   if(!session){
-  //     router.push('/auth')
-  //   }
-  // })
+  useEffect(() => {
+    if (!status === 'loading' && !session) {
+      router.push('/auth')
+    }
+  })
 
-  // if(status === 'loading'){
-  //   return <CircularProgress />
-  // }
+  if (status === 'loading') {
+    return <CircularProgress />
+  }
 
-  // if(!session){
-  //   return ''
-  // }
+  if (!session) {
+    return ''
+  }
+
+  async function deleteAccountHandler() {
+
+    setDeleteLoader(true)
+
+    try {
+      await deleteMethod(`/api/profile/delete-account?email=${router?.query.me}`)
+
+      setDeleteLoader(false)
+      router.push('/')
+      signOut()
+    } catch (error) {
+      setAlert(error.message)
+      setDeleteLoader(false)
+    }
+  }
 
   return (
     <div className={style.main}>
@@ -32,6 +53,17 @@ export default function UserProfile(props) {
         className={style.button}
         onClick={() => signOut()}
       >Sign Out</button>
+
+      <button
+        className={style.button}
+        onClick={() => null}
+      >Change Password</button>
+
+      <button
+        className={style.button}
+        onClick={deleteAccountHandler}
+        disabled={deleteLoader}
+      >{deleteLoader ? <CircularProgress size={20} /> : 'Delete Account'}</button>
 
       <TrackOrders orderId={orderId} orderDetails={orderDetails} />
     </div>
