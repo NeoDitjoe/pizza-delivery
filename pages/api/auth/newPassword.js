041cbaf77c6595659e53a8df768b1ecc780efb1a
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-03-25 06:36:18 +0200

    error handler

--- pages/api/auth/newPassword.js
+++ pages/api/auth/newPassword.js
@@ -10,7 +10,7 @@ export default async function handler(req, res){
       await setNewPassword(email, code, password, res)
       res.status(200).json({ message: 'success' })
     } catch (error) {
-      res.status(500).json({ message: 'Something went wrong'})
+      res.status(417).json({ message: 'Something went wrong'})
     }
   }
 }
